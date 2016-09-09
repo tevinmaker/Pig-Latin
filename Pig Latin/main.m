d841d936796ca239642f6657921955f4abc52f93
tevinmaker <tevin.maker@gmail.com>
2016-09-09 12:02:27 -0700

    set up

---
+++ Pig Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pig Latin
+//
+//  Created by Tevin Maker on 2016-09-09.
+//  Copyright © 2016 Tevin Maker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
